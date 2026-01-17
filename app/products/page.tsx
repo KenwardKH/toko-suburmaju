b68da33c934f379ef1a3923bb529eaf820923e85
KenwardKH <kenwardkh5@gmail.com>
2026-01-17 22:58:09 +0700

    `Added Suspense component to ProductPage to handle loading state`

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,9 +1,23 @@
 import ProductList from "@/components/products/ProductList";
+import { Suspense } from "react";
 
 export const metadata = {
     title: "Daftar Produk"
 }
 
 export default function ProductPage(){
-    return <ProductList />
+    return (
+    <div className="min-h-screen bg-gray-50">
+      {/* 👇 2. Bungkus ProductList dengan Suspense */}
+      <Suspense 
+        fallback={
+          // Tampilan sementara saat Next.js membaca URL (Loading...)
+          <div className="flex justify-center items-center h-96">
+            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-yellow-500"></div>
+          </div>
+        }
+      >
+        <ProductList />
+      </Suspense>
+    </div>)
 }
